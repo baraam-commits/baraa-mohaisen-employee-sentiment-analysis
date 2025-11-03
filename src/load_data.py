5cb69a0f4098bffcd2973782bb98be60359478d0
Baraa Mohaisen <mohaisenbaraa@gmail.com>
2025-11-02 19:23:09 -0800

    Updated how precleaned data is loaded

--- src/load_data.py
+++ src/load_data.py
@@ -9,24 +9,29 @@ class LoadData:
         data_frame = pd.read_csv(self.file_path)
         
         # helper function to orginze and clean the dataframe.
-        def _clean_df(df):
-            # Combine Text: get rid of unnecessary spaces
-            df["text"] = (df["Subject"].fillna("") + " " + df["body"].fillna("")).str.strip()
+        def _prep_df(df):
             
             # Convert date to datetime and sort by order for downstream processing
             df["dt"] = pd.to_datetime(df["date"], errors="coerce", infer_datetime_format=True)
             df = df.dropna(subset=["dt"])
             df = df.sort_values(["from", "dt"], kind="mergesort").reset_index(drop=True)
             
-            # Normalize employee IDs by extracting from email addresses
-            df["employee_id"] = df["from"].str.extract(r"([^@]+)").iloc[:,0].str.lower()
 
-            # drop any duplicates
-            df = df.drop_duplicates(subset=["employee_id", "dt", "text"])
+            
+            if clean:
 
-            # Strip any newlines, multiple spaces, and non printables
-            import re
-            df["text"] = df["text"].apply(lambda s: re.sub(r"\s+", " ", s.strip()) if isinstance(s, str) else "")
-            return df
+                # Combine Text: get rid of unnecessary spaces
+                df["text"] = (df["Subject"].fillna("") + " " + df["body"].fillna("")).str.strip()
+                
+                # Normalize employee IDs by extracting from email addresses
+                df["employee_id"] = df["from"].str.extract(r"([^@]+)").iloc[:,0].str.lower()
+                
+                # drop any duplicates
+                df = df.drop_duplicates(subset=["employee_id", "dt", "text"])
 
-        return _clean_df(data_frame) if clean else data_frame
+                # Strip any newlines, multiple spaces, and non printables
+                import re
+                df["text"] = df["text"].apply(lambda s: re.sub(r"\s+", " ", s.strip()) if isinstance(s, str) else "")
+                return df
+
+        return _prep_df(data_frame)
